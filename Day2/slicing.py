6877ef608caf6e625cd182f9a1a9a9365ed5e3be
Krishnaprasad Sunkoji <krishnasunkoji.dev@gmail.com>
2026-02-08 14:32:27 +0530

    Delete Day2/slicing.py

--- Day2/slicing.py
+++
@@ -1,8 +0,0 @@
-name = "Krishnaprasad"
-
-print(name[:]) # form start to end
-print(name[:5]) # from start(inclusive) to 4th index 5th is exclusive
-print(name[5:]) # from 5th(inclusive) index to end of the string
-print(name[5:9]) # from 5th(inclusive) index to 8th index 9th(exclusive)
-
-# [start : end] -->> end is Exclusive
