99cfe1aeacbb2f7b2516653fc0e4a5ea0ec66708
curly-engine <krishnasunkoji.dev@gmail.com>
2026-02-08 14:01:07 +0530

    indexing

---
+++ Day2/task1/indexing.py
@@ -0,0 +1,6 @@
+name = "Krishnaprasad"
+
+print(name[0]) # first char of string
+print(name[1]) #second char of string
+print(name[-1]) #first char from reverse order
+print(name[-2]) #second char from reverse order
